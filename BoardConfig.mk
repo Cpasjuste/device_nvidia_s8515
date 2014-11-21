e2fe7d47c152e60031f196893414f578770f85f9
cpasjuste <cpasjuste@gmail.com>
2014-11-21 17:20:03 +0100

    cwm recovery test #1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,6 +111,17 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path
 WIFI_DRIVER_MODULE_ARG           := "iface_name=wlan0"
 WIFI_DRIVER_MODULE_NAME          := "bcmdhd"
 
+# CWM RECOVERY
+BOARD_HAS_NO_SELECT_BUTTON := true
+BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_23x41.h\"
+TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/devices/platform/pwm-backlight/backlight/pwm-backlight/brightness\"
+BOARD_UMS_LUNFILE := "/sys/devices/platform/tegra-udc.0/gadget/lun0/file"
+COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/nvidia/s8515/recovery/recovery_keys.c
+TARGET_RECOVERY_FSTAB := device/nvidia/s8515/recovery.fstab
+BOARD_CUSTOM_GRAPHICS := ../../../device/nvidia/ss8515/recovery/cwm-graphics.c
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
 # TWRP RECOVERY
 TW_BOARD_CUSTOM_GRAPHICS := ../../../device/nvidia/s8515/recovery/graphics.c
 DEVICE_RESOLUTION := 720x1280
